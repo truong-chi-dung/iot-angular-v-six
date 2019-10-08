386bb85a9bde2b2dcef40cb3c4d07f7edfc409bc
Truong Chi Dung <B600038524@NCV-NIDEC.nidec.co.jp>
2019-10-08 16:39:24 +0700

    add host address

--- src/app/@service/device.service.ts
+++ src/app/@service/device.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Device } from '../@model/device';
+import { HostAddress } from '../@model/host';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,8 @@ export class DeviceService {
 
   constructor(private http: HttpClient) { }
 
-  getDevicesUrl = 'http://localhost:8080/devices/';
+  // getDevicesUrl = 'http://localhost:8080/devices/';
+  getDevicesUrl = HostAddress.API_ENDPOINT + 'devices/';
 
   getDevices(): Observable<Device[]> {
     return this.http.get<Device[]>(this.getDevicesUrl);
--- src/app/@service/partcount.service.ts
+++ src/app/@service/partcount.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { PartCount } from '../@model/partcount';
 import { Observable, BehaviorSubject } from 'rxjs';
+import { HostAddress } from '../@model/host';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,9 @@ export class PartcountService {
 
   constructor(private http: HttpClient) { }
 
-  getPartCountUrl = 'http://localhost:8080/streams/partcount/period/';
+  // getPartCountUrl = 'http://localhost:8080/streams/partcount/period/';
+
+  getPartCountUrl = HostAddress.API_ENDPOINT + 'streams/partcount/period/';
 
   getPartCountByDeviceId(id: string): Observable<PartCount[]> {
     return this.http.get<PartCount[]>(this.getPartCountUrl + id);
