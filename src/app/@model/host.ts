66c4840eebb02dbd4d3aee5f23aa54ecf81c9b04
Truong Chi Dung <B600038524@NCV-NIDEC.nidec.co.jp>
2019-10-17 10:59:05 +0700

    adjust api endpoint

--- src/app/@model/host.ts
+++ src/app/@model/host.ts
@@ -1,4 +1,4 @@
 export class HostAddress {
-    public static API_ENDPOINT='http://192.168.1.138:8080/';    
-    // public static API_ENDPOINT='http://localhost:8080/';
+    // public static API_ENDPOINT='http://192.168.1.138:8080/';    
+    public static API_ENDPOINT='http://localhost:8080/api/';
  }
